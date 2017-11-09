2dea46577115ffc6facb431a90a63158698f8b8b
mixelpixel <mixelpix@mac.com>
2017-11-09 13:50:53 -0500

    initial setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
+/******************************************************************************
+ * LS - Secret-Gists
+ *
+ * Patrick Kennedy
+ ******************************************************************************/
+'use-strict';
+/* eslint no-console: 0 */
+
 const express = require('express');
 const github = require('github');
+console.log(github); // <~~~ just making linter happy for now by using github variable
 
 const server = express();
 
