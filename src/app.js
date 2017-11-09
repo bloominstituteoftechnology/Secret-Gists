ded796fa78d800ff8dbe88c1bd8b739c95143b85
soycode <soycode@users.noreply.github.com>
2017-11-09 11:13:55 -0800

    add some initial github operations to support

--- src/app.js
+++ src/app.js
@@ -3,4 +3,13 @@ const github = require('github');
 
 const server = express();
 
+server.post('/login', (req, res) => {
+    const { username, oauth_token } = req.body;
+    // TODO log in to GitHub, return success/failure response
+});
+
+server.get('/gists', (req, res) => {
+    // TODO retrieve a list of gists for the currently authed user
+});
+
 server.listen(3000);
