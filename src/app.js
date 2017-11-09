0185d99a842f9de14f53eeb6e08b129f17deaec2
soycode <soycode@users.noreply.github.com>
2017-11-09 11:17:02 -0800

    add .eslintrc.json file

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ const github = require('github');
 const server = express();
 
 server.post('/login', (req, res) => {
-    const { username, oauth_token } = req.body;
-    // TODO log in to GitHub, return success/failure response
+  const { username, oauth_token } = req.body;
+  // TODO log in to GitHub, return success/failure response
 });
 
 server.get('/gists', (req, res) => {
-    // TODO retrieve a list of gists for the currently authed user
+  // TODO retrieve a list of gists for the currently authed user
 });
 
 server.listen(3000);
