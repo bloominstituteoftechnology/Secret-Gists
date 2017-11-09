911253d412cdd1048fba6fbd04c0526b4994c822
mixelpixel <mixelpix@mac.com>
2017-11-09 17:11:17 -0500

    ...

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
-/******************************************************************************
+/* ****************************************************************************
  * LS - Secret-Gists
  * let users authenticate with GitHub (rather than maintaining your own user data)
  * and save/retrieve gists
  * Patrick Kennedy
- ******************************************************************************/
+ **************************************************************************** */
+
 'use-strict';
+
 /* eslint no-console: 0 */
 
 const express = require('express');
 const github = require('github');
-console.log(github); // <~~~ just making linter happy for now by using github variable
+
+// console.log(github); // <~~~ just making linter happy for now by using github variable
 
 const server = express();
 
