2f5efbfb43a0508a8ae4ce6f4cdfa86ff992ada1
mixelpixel <mixelpix@mac.com>
2017-11-09 20:28:50 -0500

    console logging tidbits from readme

--- src/app.js
+++ src/app.js
@@ -10,14 +10,34 @@
 /* eslint no-console: 0 */
 
 const express = require('express');
-const github = require('github');
+const Github = require('github');
+const nacl = require('tweetnacl');
+nacl.util = require('tweetnacl-util');
 
-// console.log(github); // <~~~ just making linter happy for now by using github variable
+// console.log(Github);
+
+// console.log('1.) nacl\n', nacl, '\n', '2.) nacl.util\n', nacl.util);
 
 const server = express();
 
+const githubCli = new Github({
+  baseUri: 'https://api.github.com',
+  token: process.env.GITHUB_TOKEN
+});
+
+// console.log('1.) process.env\n', process.env, '\n');
+// console.log('2.) process.env.GITHUB_TOKEN\n', process.env.GITHUB_TOKEN, '\n');
+// console.log('githubCli \n', githubCli);
+
+// let handle = 'k33g';
+const handle = 'mixelpixel';
+githubCli.users.getForUser({ username: handle }).then((response) => {
+  console.log('\n1.) response.data\n', response.data);
+});
+
 server.post('/login', (req, res) => {
   const { username, oauth_token } = req.body;
+  console.log(username, oauth_token);
   // TODO log in to GitHub, return success/failure response
 });
 
