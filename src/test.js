4d3357795819f3e6a9ef52ece394b8afd048a5f0
Neil Barduson <barduson.neil@gmail.com>
2018-01-23 16:03:02 -0600

    spent a decent amount of time with env variables and how to write to them, ended up using a json file to persist the key

---
+++ src/test.js
@@ -0,0 +1,5 @@
+
+// console.log(process.env.GITHUB_TOKEN);
+require('dotenv').config({ path: '/Users/squeel/lambda/Secret-Gists/.env' })
+// require('dotenv').config()
+console.log(process.env.TEST)
