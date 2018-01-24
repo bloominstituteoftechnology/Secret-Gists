f721fa88df241dc7c16048e7c8ca0408c0d00ffc
Collin Ferguson <theredcouchroom@gmail.com>
2018-01-23 18:03:13 -0600

    Still working on stretch goals. Had trouble getting bodyparser to work

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const GitHubApi = require('github');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
+require('dotenv').config();
 
-const username = 'yourusername';  // TODO: your GitHub username here
+
+const username = 'coleferg';  // TODO: your GitHub username here
 const github = new GitHubApi({ debug: true });
 const server = express();
-
+server.use(bodyParser.json());
 // Generate an access token: https://github.com/settings/tokens
 // Set it to be able to create gists
 github.authenticate({
@@ -17,31 +19,73 @@ github.authenticate({
 
 // Set up the encryption - use process.env.SECRET_KEY if it exists
 // TODO either use or generate a new 32 byte key
+const key = !process.env.SECRET_KEY ? nacl.randomBytes(32) : nacl.util.encodeBase64(process.env.SECRET_KEY);
 
 server.get('/', (req, res) => {
   // TODO Return a response that documents the other routes/operations available
+  const response = 'Welcome\n/gists\n/key\n/secretgist/:id\n/create\n/createsecret\n';
+  res.send(response);
 });
 
 server.get('/gists', (req, res) => {
   // TODO Retrieve a list of all gists for the currently authed user
+  github.gists.getForUser({ username }).then((result) => {
+    res.send(result.data);
+  });
 });
 
 server.get('/key', (req, res) => {
   // TODO Return the secret key used for encryption of secret gists
+  res.send(nacl.util.encodeBase64(key));
 });
 
 server.get('/secretgist/:id', (req, res) => {
   // TODO Retrieve and decrypt the secret gist corresponding to the given ID
+  const id = req.params.id;
+  github.gists.get({ id })
+    .then((result) => {
+      const gist = result.data;
+      const filename = Object.keys(gist.files)[0];
+      const blob = gist.files[filename].content;
+      const nonce = nacl.util.decodeBase64(blob.slice(0, 32));
+      const ciphertext = nacl.util.decodeBase64(blob.slice(32, blob.length));
+      const plaintext = nacl.secretbox.open(ciphertext, nonce, key);
+      res.send(nacl.util.encodeUTF8(plaintext));
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 server.post('/create', (req, res) => {
   // TODO Create a private gist with name and content given in post request
+  const { name, content } = req.body;
+  const files = { [name]: { content } };
+  github.gists.create({ files, public: false })
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 server.post('/createsecret', (req, res) => {
   // TODO Create a private and encrypted gist with given name/content
   // NOTE - we're only encrypting the content, not the filename
   // To save, we need to keep both encrypted content and nonce
+  const { name, content } = req.body;
+  const nonce = nacl.randomBytes(24);
+  const ciphertext = nacl.secretbox(nacl.util.decodeUTF8(content), nonce, key);
+  const blob = nacl.util.encodeBase64(nonce) + nacl.util.encodeBase64(ciphertext);
+  const files = { [name]: { content: blob } };
+  github.gists.create({ files, public: false })
+    .then((result) => {
+      res.json(result.data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 /* OPTIONAL - if you want to extend functionality */
