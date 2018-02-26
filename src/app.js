07aaeb6df9a8d165d86c5e9881164d0946e7514a
MannieJay <jhall02258@gmail.com>
2018-02-26 17:46:45 -0500

    get /gists and partial key

--- src/app.js
+++ src/app.js
@@ -4,15 +4,19 @@ const GitHubApi = require('github');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'yourusername';  // TODO: your GitHub username here
+require('dotenv').config();
+
+const username = 'Lambdarines';  // TODO: your GitHub username here
 const github = new GitHubApi({ debug: true });
 const server = express();
-
+const token = process.env.GITHUB_TOKEN;
+let client_id = '';
+console.log('token', token);
 // Generate an access token: https://github.com/settings/tokens
 // Set it to be able to create gists
 github.authenticate({
   type: 'oauth',
-  token: process.env.GITHUB_TOKEN
+  token
 });
 
 // Set up the encryption - use process.env.SECRET_KEY if it exists
@@ -20,52 +24,60 @@ github.authenticate({
 
 server.get('/', (req, res) => {
   // TODO Return a response that documents the other routes/operations available
-  let handle = "invegat";
-  github.users.getForUser({ username: handle }).then(response => {
-
+  github.users.getForUser({ username }).then((response) => {
     res.json(response.data);
+    console.log("client id", response.data.id);
+    client_id = response.data.id;
     // console.log(response.data);
   });
+});
 
+server.get('/gists', (req, res) => {
+  // TODO Retrieve a list of all gists for the currently authed user
+  github.gists.getForUser({ username })
+    .then(response => {
+      res.json(response.data);
+    });
+});
 
-  server.get('/gists', (req, res) => {
-    // TODO Retrieve a list of all gists for the currently authed user
-  });
+server.get('/key', (req, res) => {
+  // TODO Return the secret key used for encryption of secret gists
+  github.authorization.check({ access_token: token, client_id: client_id }).then(result => {
+    res.json(response.data)
+  })
+  console.log()
+});
 
-  server.get('/key', (req, res) => {
-    // TODO Return the secret key used for encryption of secret gists
-  });
+server.get('/secretgist/:id', (req, res) => {
+  // TODO Retrieve and decrypt the secret gist corresponding to the given ID
+});
 
-  server.get('/secretgist/:id', (req, res) => {
-    // TODO Retrieve and decrypt the secret gist corresponding to the given ID
-  });
+server.post('/create', (req, res) => {
+  // TODO Create a private gist with name and content given in post request
+});
 
-  server.post('/create', (req, res) => {
-    // TODO Create a private gist with name and content given in post request
-  });
+server.post('/createsecret', (req, res) => {
+  // TODO Create a private and encrypted gist with given name/content
+  // NOTE - we're only encrypting the content, not the filename
+  // To save, we need to keep both encrypted content and nonce
+});
 
-  server.post('/createsecret', (req, res) => {
-    // TODO Create a private and encrypted gist with given name/content
-    // NOTE - we're only encrypting the content, not the filename
-    // To save, we need to keep both encrypted content and nonce
-  });
+/* OPTIONAL - if you want to extend functionality */
+server.post('/login', (req, res) => {
+  // TODO log in to GitHub, return success/failure response
+  // This will replace hardcoded username from above
+  // const { username, oauth_token } = req.body;
+  res.json({ success: false });
+});
 
-  /* OPTIONAL - if you want to extend functionality */
-  server.post('/login', (req, res) => {
-    // TODO log in to GitHub, return success/failure response
-    // This will replace hardcoded username from above
-    // const { username, oauth_token } = req.body;
-    res.json({ success: false });
-  });
+/*
+Still want to write code? Some possibilities:
+-Pretty templates! More forms!
+-Better management of gist IDs, use/display other gist fields
+-Support editing/deleting existing gists
+-Switch from symmetric to asymmetric crypto
+-Exchange keys, encrypt messages for each other, share them
+-Let the user pass in their private key via POST
+*/
 
-  /*
-  Still want to write code? Some possibilities:
-  -Pretty templates! More forms!
-  -Better management of gist IDs, use/display other gist fields
-  -Support editing/deleting existing gists
-  -Switch from symmetric to asymmetric crypto
-  -Exchange keys, encrypt messages for each other, share them
-  -Let the user pass in their private key via POST
-  */
-
-  server.listen(3000);
+server.listen(3000);
