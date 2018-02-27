93534391aa18921ff3df7065cc984df2b6562423
David Long <davdaarn@live.com>
2018-02-27 12:31:54 -0600

    daily

--- src/app.js
+++ src/app.js
@@ -1,33 +1,63 @@
-const bodyParser = require('body-parser');
+const nacl = require('tweetnacl');
 const express = require('express');
 const GitHubApi = require('github');
-const nacl = require('tweetnacl');
+const bodyParser = require('body-parser');
+
 nacl.util = require('tweetnacl-util');
 
-const username = 'yourusername';  // TODO: your GitHub username here
+const username = 'davdaarn'; // TODO: your GitHub username here
+const token = require('../token');
 const github = new GitHubApi({ debug: true });
 const server = express();
+
+server.use(bodyParser.json());
 
 // Generate an access token: https://github.com/settings/tokens
 // Set it to be able to create gists
 github.authenticate({
   type: 'oauth',
-  token: process.env.GITHUB_TOKEN
+  token: token.GITHUB_TOKEN,
+  // token: process.env.GITHUB_TOKEN
 });
 
 // Set up the encryption - use process.env.SECRET_KEY if it exists
 // TODO either use or generate a new 32 byte key
 
+server.get('/test', (req, res) => {
+  // TODO Retrieve a list of all gists for the currently authed user
+  console.log(token.GITHUB_TOKEN);
+  github.users.getForUser({ username }).then(response => {
+    // console.log(response.data);
+    res.json(response.data);
+  });
+});
+
 server.get('/', (req, res) => {
   // TODO Return a response that documents the other routes/operations available
+  res.json({
+    '/gists': '',
+    '/key': '',
+    '/secretgist/:id': '',
+    '/create': '',
+    '/createsecret': '',
+    '/login': '',
+  });
 });
 
 server.get('/gists', (req, res) => {
   // TODO Retrieve a list of all gists for the currently authed user
+  github.users.getForUser({ username }).then(response => {
+    console.log(response.data);
+  });
 });
 
 server.get('/key', (req, res) => {
   // TODO Return the secret key used for encryption of secret gists
+  github.authorization
+    .check({ access_token: token, client_id: client_id })
+    .then(result => {
+      res.json(response.data);
+    });
 });
 
 server.get('/secretgist/:id', (req, res) => {
@@ -36,6 +66,8 @@ server.get('/secretgist/:id', (req, res) => {
 
 server.post('/create', (req, res) => {
   // TODO Create a private gist with name and content given in post request
+  const test = { key, content: 'test', name: 'test name' };
+  github.gists.create(test, true, 'Test Gist');
 });
 
 server.post('/createsecret', (req, res) => {
