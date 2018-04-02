979a58d3a2d6382aee1ee3debf4ffa1d4f0f05ff
Vinnie <vinnie.scalco@gmail.com>
2018-04-02 13:32:02 -0500

    complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'yourusername';  // TODO: your GitHub username here
+const username = 'VinnieScalco';  // TODO: your GitHub username here
 const github = new octokit({ debug: true });
 const server = express();
 
@@ -21,14 +21,28 @@ github.authenticate({
 
 server.get('/', (req, res) => {
   // TODO Return a response that documents the other routes/operations available
+  github.users.getForUser({
+    username
+  }).then(response => {
+    res.json(response.data);
+    console.log("client id", response.data.id);
+    client_id = response.data.id;
+    // console.log(response.data);
+  });
 });
 
 server.get('/gists', (req, res) => {
   // TODO Retrieve a list of all gists for the currently authed user
+  github.gists.getForUser({
+    username
+  }).then(response => {
+    res.json(response.data);
+  });
 });
 
 server.get('/key', (req, res) => {
   // TODO Return the secret key used for encryption of secret gists
+  res.json(boxKey.toString());
 });
 
 server.get('/secretgist/:id', (req, res) => {
@@ -37,12 +51,49 @@ server.get('/secretgist/:id', (req, res) => {
 
 server.post('/create', (req, res) => {
   // TODO Create a private gist with name and content given in post request
+  github.gists.create({
+    key: "key",
+    public: true,
+    description: "My first gist",
+    files: {
+      "file1.txt": {
+        content: "Aren't gists great!"
+      }
+    }
+  },
+    () => res.json({
+      status: "done"
+    })
+  );
 });
 
 server.post('/createsecret', (req, res) => {
   // TODO Create a private and encrypted gist with given name/content
   // NOTE - we're only encrypting the content, not the filename
   // To save, we need to keep both encrypted content and nonce
+  let pair = nacl.sign.keyPair();
+  myPublicKey = pair.publicKey;
+  let newNonce = () => nacl.randomBytes(24);
+  let newBoxKey = () => nacl.randomBytes(32);
+  nonce = newNonce();
+  boxKey = newBoxKey();
+  console.log("nonce", nonce, boxKey);
+  let message = nacl.util.decodeUTF8('encrypt the stupid thing!');
+  let encMessage = nacl.secretbox(message, nonce, boxKey);
+  github.gists.create({
+    public: false,
+    description: "THE secret gist",
+    files: {
+      'file6.txt': {
+        content: nacl.util.encodeBase64(encMessage)
+      }
+    }
+  },
+    () => res.json({
+      content: nacl.util.encodeBase64(encMessage),
+      status: 'done'
+    })
+  )
 });
 
 /* OPTIONAL - if you want to extend functionality */
