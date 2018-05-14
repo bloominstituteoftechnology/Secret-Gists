a2455ba85661426da709540f5737535f974fb1c2
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-14 17:38:26 -0500

    Server Listen
    - in app.js:
    - add syntax for detecting OR assigning a port, listen, and console success
    - use eslint rule disable in-line for console exception

--- src/app.js
+++ src/app.js
@@ -190,4 +190,7 @@ Still want to write code? Some possibilities:
 -Let the user pass in their private key via POST
 */
 
-server.listen(3000);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on ${port}`); // eslint-disable-line no-console
+});
