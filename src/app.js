f61c30aecb3d5e1f23f22bfe784cd17ecc31c239
Jamie John David <jjdavid14@live.com>
2018-06-11 14:08:25 -0700

    Completed keyPairGen - /key - /setKey:

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
-
+const key = process.env.SECRET_KEY ? nacl.util.decodeBase64(process.env.SECRET_KEY) : nacl.randomBytes(32);
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
@@ -78,6 +78,7 @@ server.get('/', (req, res) => {
 
 server.get('/keyPairGen', (req, res) => {
   // TODO:  Generate a keypair from the secretKey and display both
+  const keypair = nacl.box.keyPair.fromSecretKey(secretKey);
 
   // Display both keys as strings
   res.send(`
@@ -115,6 +116,8 @@ server.get('/setkey:keyString', (req, res) => {
   const keyString = req.query.keyString;
   try {
     // TODO:
+    secretKey = nacl.util.decodeBase64(keyString);
+    res.send(secretKey);
   } catch (err) {
     // failed
     res.send('Failed to set key.  Key string appears invalid.');
