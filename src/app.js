4dcc63b440a7b11adf6b078e9ff391a56ffe5b5e
William <wtpwinberg@gmail.com>
2018-06-12 11:05:27 -0700

    bug back

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ github.authenticate({
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
 const key = process.env.CLIENT_SECRET ? nacl.util.decodeBase64(process.env.CLIENT_SECRET) : nacl.randomBytes(32);
+// const key = nacl.randomBytes(32);
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
