3c3f43a8372d16c6f01d64cb3bbc759a087affd5
sophiemullerc <32464200+sophiemullerc@users.noreply.github.com>
2018-06-14 09:07:02 -0700

    WIP Sophie MUller

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'yourusername';  // TODO: your GitHub username here
+const username = 'sophiemullerc';  // TODO: your GitHub username here
 const github = new octokit({ debug: true });
 const server = express();
 
@@ -18,6 +18,25 @@ github.authenticate({
 
 // Set up the encryption - use process.env.SECRET_KEY if it exists
 // TODO either use or generate a new 32 byte key
+  const data = fs.readFileSync('./config.json');
+  let secretKey;
+
+    try {
+      const keyObject = JSON.parse(data);
+      secretKey = nacl.util.decodeBase64(keyObject.secretKey);
+    
+    } catch (err) {
+      secretKey = nacl.randomBytes(32);
+  
+      const keyObject = { secretKey: nacl.util.encodeBase64(secretKey) };
+          fs.writeFile('./config.json', JSON.stringify(keyObject), (ferr) => {
+         if (ferr) {
+             console.log('There has been an error saving the key data.');
+            console.log(err.message);
+           return;
+          }
+      });
+    }
 
 server.get('/', (req, res) => {
   // TODO Return a response that documents the other routes/operations available
@@ -25,18 +44,41 @@ server.get('/', (req, res) => {
 
 server.get('/gists', (req, res) => {
   // TODO Retrieve a list of all gists for the currently authed user
+  github.gists
+     .getForUser({ username })
+     .then((response) => {
+       res.json(response.data);
+     })
 });
 
 server.get('/key', (req, res) => {
   // TODO Return the secret key used for encryption of secret gists
+  res.send(nacl.util.encodeBase64(secretKey));
 });
 
 server.get('/secretgist/:id', (req, res) => {
   // TODO Retrieve and decrypt the secret gist corresponding to the given ID
+   const keyString = req.query.keyString;
+     try {
+         secretKey = nacl.util.decodeBase64(keyString);
+          res.send(`<div> Key set to new value: ${keyString} </div>`);
+   
+      } catch (err) {
+     
+     res.send('Failed to set key.  Key string appears invalid.');
+      }
 });
 
 server.post('/create', (req, res) => {
   // TODO Create a private gist with name and content given in post request
+   const { name, content } = req.body;
+   const files = { [name]: { content } };
+    github.gists.create({ files, public: false })
+    github.gists
+      .create({ files, public: false })
+      .then((response) => {
+       res.json(response.data);
+     })
 });
 
 server.post('/createsecret', (req, res) => {
