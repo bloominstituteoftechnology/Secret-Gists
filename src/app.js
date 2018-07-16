82b85930a38143010742ea98774917afd9f50ba4
Natalie <natalieschennum@gmail.com>
2018-07-16 12:46:50 -0700

    Completed keypair with token from Github

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'your_name_here'; // TODO: Replace with your username
+const username = 'Nschennum'; // TODO: Replace with your username
 const github = octokit({ debug: true });
 const server = express();
 
@@ -21,7 +21,6 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
-
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
