ab36b472d04a805af53c6be5f548ec4a0f1ec24a
Harrison Brock <harrison@harrisonbrock.com>
2018-07-16 16:01:00 -0400

    CODE: Removed TODO

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ github.authenticate ({
   token: process.env.GITHUB_TOKEN
 });
 
-// TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key
+//   Attempt to load the key from config.json.  If it is not found, create a new 32 byte key
 
 let privateKey;
 let CONFIG;
