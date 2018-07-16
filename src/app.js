2bbf2b3a4448165387dc2361a8733f8fbf97075d
Mark Wong <fmarkwong@gmail.com>
2018-07-16 15:13:31 -0700

    keypair

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ github.authenticate({
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
 
+const asyncHandler = fn => (req, res, next) => fn(req, res, next).catch(next);
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
@@ -76,8 +77,14 @@ server.get('/', (req, res) => {
   `);
 });
 
-server.get('/keyPairGen', (req, res) => {
+// server.get('/api/notes', asyncHandler(async (req, res) => {
+//   const response = await Note.find()
+//   res.status(200).json(response);
+// }));
+
+server.get('/keyPairGen', asyncHandler(async (req, res) => {
   // TODO:  Generate a keypair from the secretKey and display both
+  const keypair = await nacl.box.keyPair();
 
   // Display both keys as strings
   res.send(`
@@ -92,7 +99,7 @@ server.get('/keyPairGen', (req, res) => {
       <div>Secret Key: ${nacl.util.encodeBase64(keypair.secretKey)}</div>
     </body>
   `);
-});
+}));
 
 server.get('/gists', (req, res) => {
   // Retrieve a list of all gists for the currently authed user
