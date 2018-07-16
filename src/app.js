0835cca18c9d805e3e29a19cc247813070181262
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-07-16 18:03:54 -0500

    initial commit

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'your_name_here'; // TODO: Replace with your username
+const username = 'markstez05'; // TODO: Replace with your username
 const github = octokit({ debug: true });
 const server = express();
 
@@ -21,7 +21,8 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
-
+const key = process.env.secretKey ?
+    nacl.util.decodeBase64(process.env.secretKey) : nacl.randomBytes(32);
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
@@ -78,7 +79,9 @@ server.get('/', (req, res) => {
 
 server.get('/keyPairGen', (req, res) => {
   // TODO:  Generate a keypair from the secretKey and display both
-
+  const publicKey = '';
+  const secretKey = '';
+  const keypair = { publicKey, secretKey };
   // Display both keys as strings
   res.send(`
   <html>
@@ -107,6 +110,7 @@ server.get('/gists', (req, res) => {
 
 server.get('/key', (req, res) => {
   // TODO: Display the secret key used for encryption of secret gists
+  res.send(nacl.util.encodeBase64(keypair.secretKey));
 });
 
 server.get('/setkey:keyString', (req, res) => {
@@ -172,3 +176,10 @@ Still want to write code? Some possibilities:
 */
 
 server.listen(3000);
+
+// const port = process.env.PORT || 3000;
+
+// server.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
+
