f7d9c86054fd28bcb067db0afd8d8975fb79d119
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-07-17 09:48:33 -0700

    app.js - Added .env file with github token; installed deps; secret test gist created successfully

--- src/app.js
+++ src/app.js
@@ -20,6 +20,8 @@ github.authenticate({
   token: process.env.GITHUB_TOKEN
 });
 
+const keypair = {};
+
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
 
 
