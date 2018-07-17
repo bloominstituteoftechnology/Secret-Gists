653d054a29dd34618461641c646cd0499df23805
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-07-17 13:24:55 -0700

    App.js - Comment out console.log

--- src/app.js
+++ src/app.js
@@ -22,22 +22,22 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
-const data = fs.readFileSync('./config.json');
-let secretKey;
-try {
-  const keyObject = JSON.parse(data);
-  secretKey = nacl.util.decodeBase64(keyObject.secretKey);
-} catch (err) {
-  secretKey = nacl.randomBytes(32);
-  const keyObject = { secretKey: nacl.util.encodeBase64(secretKey) };
-  fs.writeFile('./config.json', JSON.stringify(keyObject), (ferr) => {
-    if (ferr) {
-      console.log('There has been an error saving the key data.');
-      console.log(err.message);
-      return;
-    }
-  });
-}
+// const data = fs.readFileSync('./config.json');
+// let secretKey;
+// try {
+//   const keyObject = JSON.parse(data);
+//   secretKey = nacl.util.decodeBase64(keyObject.secretKey);
+// } catch (err) {
+//   secretKey = nacl.randomBytes(32);
+//   const keyObject = { secretKey: nacl.util.encodeBase64(secretKey) };
+//   fs.writeFile('./config.json', JSON.stringify(keyObject), (ferr) => {
+//     if (ferr) {
+//       // console.log('There has been an error saving the key data.');
+//       // console.log(err.message);
+//       return;
+//     }
+//   });
+// }
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
