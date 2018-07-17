25ec612e9086a791533c48f88e20897fc279c134
JasonCorp84 <csababalogh84@gmail.com>
2018-07-17 15:57:28 -0700

    added some updates

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = process.env.USERNAME; // TODO: Replace with your username
+const username = 'your_name_here'; // TODO: Replace with your username
 const github = octokit({ debug: true });
 const server = express();
 
@@ -20,7 +20,29 @@ github.authenticate({
   token: process.env.GITHUB_TOKEN
 });
 
-// TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+const keypair = {};
+
+// TODO:  Attempt to load the key from config.json.  If it is not found,
+// create a new 32 byte key.
+let secretKey;
+
+try {
+	const data = fs.readFileSync('./config.json');
+
+	// Read the key from the file
+	const keyObject = JSON.parse(data);
+	secretKey = nacl.util.decodeBase64(keyObject.secretKey);
+} catch (err) {
+	// Key not found in file, so write it to the file
+	secretKey = nacl.randomBytes(32);
+	const keyObject = { secretKey: nacl.util.encodeBase64(secretKey) };
+
+	fs.writeFile('./config.json', JSON.stringify(keyObject, null, 4), (ferr) => {
+		if (ferr) {
+			console.log('Error saving config.json: ' + ferr.message);
+		}
+	});
+}
 
 
 server.get('/', (req, res) => {
@@ -39,36 +61,49 @@ server.get('/', (req, res) => {
         </ul>
         <h3>Set your secret key to a specific key</h3>
         <form action="/setkey:keyString" method="get">
-          Key String: <input type="text" name="keyString"><br>
+          Key String: <input type="text" name="keyString">
+
           <input type="submit" value="Submit">
         </form>
         <h3>Create an *unencrypted* gist</h3>
         <form action="/create" method="post">
-          Name: <input type="text" name="name"><br>
-          Content:<br><textarea name="content" cols="80" rows="10"></textarea><br>
+          Name: <input type="text" name="name">
+
+          Content:
+<textarea name="content" cols="80" rows="10"></textarea>
+
           <input type="submit" value="Submit">
         </form>
         <h3>Create an *encrypted* gist for yourself</h3>
         <form action="/createsecret" method="post">
-          Name: <input type="text" name="name"><br>
-          Content:<br><textarea name="content" cols="80" rows="10"></textarea><br>
+          Name: <input type="text" name="name">
+
+          Content:
+<textarea name="content" cols="80" rows="10"></textarea>
+
           <input type="submit" value="Submit">
         </form>
         <h3>Retrieve an *encrypted* gist you posted for yourself</h3>
         <form action="/fetchmessagefromself:id" method="get">
-          Gist ID: <input type="text" name="id"><br>
+          Gist ID: <input type="text" name="id">
+
           <input type="submit" value="Submit">
         </form>
         <h3>Create an *encrypted* gist for a friend to decode</h3>
         <form action="/postmessageforfriend" method="post">
-          Name: <input type="text" name="name"><br>
-          Friend's Public Key String: <input type="text" name="publicKeyString"><br>
-          Content:<br><textarea name="content" cols="80" rows="10"></textarea><br>
+          Name: <input type="text" name="name">
+
+          Friend's Public Key String: <input type="text" name="publicKeyString">
+
+          Content:
+<textarea name="content" cols="80" rows="10"></textarea>
+
           <input type="submit" value="Submit">
         </form>
         <h3>Retrieve an *encrypted* gist a friend has posted</h3>
         <form action="/fetchmessagefromfriend:messageString" method="get">
-          String From Friend: <input type="text" name="messageString"><br>
+          String From Friend: <input type="text" name="messageString">
+
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -77,12 +112,10 @@ server.get('/', (req, res) => {
 });
 
 server.get('/keyPairGen', (req, res) => {
-  const keypair = {};
-
   // TODO:  Generate a keypair from the secretKey and display both
 
   // Display both keys as strings
-  /*res.send(`
+  res.send(`
   <html>
     <header><title>Keypair</title></header>
     <body>
@@ -93,7 +126,7 @@ server.get('/keyPairGen', (req, res) => {
       <div>Public Key: ${nacl.util.encodeBase64(keypair.publicKey)}</div>
       <div>Secret Key: ${nacl.util.encodeBase64(keypair.secretKey)}</div>
     </body>
-  `);*/
+  `);
 });
 
 server.get('/gists', (req, res) => {
@@ -124,6 +157,23 @@ server.get('/setkey:keyString', (req, res) => {
 
 server.get('/fetchmessagefromself:id', (req, res) => {
   // TODO:  Retrieve and decrypt the secret gist corresponding to the given ID
+  const id = req.query.id;
+
+  github.gists.get({id}).then((response) => {
+	  const gist = response.data;
+	  const filename = Object.keys(gist.files)[0];
+
+	  const blob = gist.files[filename].content;
+
+	  let [nonce, ciphertext] = blob.split(' ');
+
+	  nonce = nacl.util.decodeBase64(nonce);
+	  ciphertext = nacl.util.decodeBase64(ciphertext);
+
+	  const plaintext = nacl.secretbox.open(ciphertext, nonce, secretKey);
+
+	  res.send(nacl.util.encodeUTF8(plaintext));
+  });
 });
 
 server.post('/create', urlencodedParser, (req, res) => {
@@ -142,6 +192,23 @@ server.post('/create', urlencodedParser, (req, res) => {
 server.post('/createsecret', urlencodedParser, (req, res) => {
   // TODO:  Create a private and encrypted gist with given name/content
   // NOTE - we're only encrypting the content, not the filename
+  const { name, content } = req.body;
+
+  const nonce = nacl.randomBytes(24);
+  
+  const ciphertext = nacl.secretbox(nacl.util.decodeUTF8(content), nonce, secretKey);
+
+  const blob = nacl.util.encodeBase64(nonce) + ' ' +
+  	nacl.util.encodeBase64(ciphertext);
+
+  const files = { [name]: { content: blob } };
+  github.gists.create({ files, public: false })
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 server.post('/postmessageforfriend', urlencodedParser, (req, res) => {
@@ -173,4 +240,4 @@ Still want to write code? Some possibilities:
 -Let the user pass in their private key via POST
 */
 
-server.listen(3000, ()=> console.log('server listens at port:3000'));
+server.listen(3000);
