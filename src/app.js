89a48c5307e86dddc372aad47873336296e689aa
Yasin Shuman <email@yasin.io>
2018-07-17 16:46:11 -0700

    some fixes - thx beej

--- src/app.js
+++ src/app.js
@@ -21,13 +21,19 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+const data = fs.readFileSync("s./config.json");
 let secretKey;
-let CONFIG;
+
 try {
-  CONFIG = JSON.parse(fs.readFileSync("config.json", "utf8"));
-  secretKey = nacl.util.decodeUTF8(CONFIG.secretKey);
+  const keyObject = JSON.parse(data);
 } catch (err) {
   secretKey = nacl.randomBytes(32);
+  const keyObject = { secretKey: nacl.util.encodeBase64(secretKey) };
+  fs.writeFile("./config.json", JSON.stringify(keyObject), fsErr => {
+    if (fsErr) {
+      console.log("Error saving config.json: " + fsErr.message);
+    }
+  });
 }
 
 server.get("/", (req, res) => {
@@ -87,6 +93,8 @@ server.get("/keyPairGen", (req, res) => {
   // TODO:  Generate a keypair from the secretKey and display both
   const keypair = nacl.box.keyPair.fromSecretKey(secretKey);
   // Display both keys as strings
+  secretKey = JSON.stringify(nacl.util.encodeBase64(keypair.secretKey));
+  publicKey = JSON.stringify(nacl.util.encodeBase64(keypair.publicKey));
   res.send(`
   <html>
     <header><title>Keypair</title></header>
@@ -95,8 +103,8 @@ server.get("/keyPairGen", (req, res) => {
       <div>Share your public key with anyone you want to be able to leave you secret messages.</div>
       <div>Keep your secret key safe.  You will need it to decode messages.  Protect it like a passphrase!</div>
       <br/>
-      <div>Public Key: ${nacl.util.encodeBase64(keypair.publicKey)}</div>
-      <div>Secret Key: ${nacl.util.encodeBase64(keypair.secretKey)}</div>
+      <div>Public Key: ${publicKey}</div>
+      <div>Secret Key: ${secretKey}</div>
     </body>
   `);
 });
@@ -133,17 +141,47 @@ server.get("/setkey:keyString", (req, res) => {
 
 server.get("/fetchmessagefromself:id", (req, res) => {
   // TODO:  Retrieve and decrypt the secret gist corresponding to the given ID
-  const id = req.query.id;
-  github.gists.get({ id }).then(response => {
-    const gist = response.data;
-    const filename = Object.keys(gist.files)[0];
-    const blob = gist.files[filename].content;
-    const [nonce, ciphertext] = blob.split(" ");
-    nonce = nacl.util.decodeBase64(nonce);
-    ciphertext = nacl.util.decodeBase64(ciphertext);
+  //   const { id } = req.query;
+  //   github.gists.get({ id })
+  //     .then(response => {
+  //     const gist = response.data;
+  //     const filename = Object.keys(gist.files)[0];
+  //     const blob = gist.files[filename].content;
+  //     const [nonce, ciphertext] = blob.split(" ");
+  //     nonce = nacl.util.decodeBase64(nonce);
+  //     ciphertext = nacl.util.decodeBase64(ciphertext);
 
-    const plaintext = nacl.secretbox.open(ciphertext, nonce, secretKey);
-  });
+  //     const plaintext = nacl.secretbox.open(ciphertext, nonce, secretKey);
+  //   });
+  // });
+  const { id } = req.query;
+  let responseFiles;
+  let responseContent;
+  // console.log('The id provided is:', id);
+  // TODO: Figure how to get a gist by ID
+  github.gists
+    .get({ id })
+    .then(response => {
+      responseFiles = response.data.files;
+      for (file in responseFiles) {
+        responseContent = responseFiles[file].content;
+      }
+
+      let nonce = responseContent.slice(0, 32);
+      nonce = nacl.util.decodeBase64(nonce);
+
+      let message = responseContent.slice(32, responseContent.length); // Extract Message
+      message = nacl.util.decodeBase64(message);
+
+      const decypheredBox = nacl.secretbox.open(message, nonce, secretKey);
+
+      const utf8DecypheredBox = nacl.util.encodeUTF8(decypheredBox);
+
+      res.json({ deciphered: utf8DecypheredBox });
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 server.post("/create", urlencodedParser, (req, res) => {
@@ -166,6 +204,23 @@ server.post("/createsecret", urlencodedParser, (req, res) => {
 
   const { name, content } = req.body;
   const nonce = nacl.randomBytes(nonceLength);
+  const encodedContent = nacl.util.decodeUTF8(content);
+  const ecryptedContent = nacl.secretbox(encodedContent, nonce, secretKey);
+
+  const utf8EncryptedContent = nacl.util.encodeBase64(ecryptedContent);
+  const utf8EncryptedNonce = nacl.util.encodeBase64(nonce);
+
+  const files = {
+    [name]: { content: `${utf8EncryptedNonce}${utf8EncryptedContent}` }
+  };
+  github.gists
+    .create({ files, public: false })
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 server.post("/postmessageforfriend", urlencodedParser, (req, res) => {
