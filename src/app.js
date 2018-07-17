30ce3a606c73c325435cbba4937abdcf6ecbb1f9
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-07-17 09:52:34 -0700

    app.js - Added empty keypair to prevent eslint error

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ github.authenticate({
   type: 'oauth',
   token: process.env.GITHUB_TOKEN
 });
-
+// Added empty keypair so eslint would stop throwing error
 const keypair = {};
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
