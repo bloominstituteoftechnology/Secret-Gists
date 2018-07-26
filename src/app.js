8482e78a36bfe8c27adfd17cbec8f0c26622e23f
Sean Chen <skypemaster007@gmail.com>
2018-07-26 15:33:49 -0500

    Remove an empty line

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ github.authenticate({
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
 
-
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
   res.send(`
