51d2d797c845fbf8985ad3c383f833e120c3f589
sagardesai90 <saagardesai@gmail.com>
2018-07-30 18:37:00 -0500

    added mySecretKey const

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'your_name_here'; // TODO: Replace with your username
+const username = 'sagardesai90'; // TODO: Replace with your username
 // The object you'll be interfacing with to communicate with github
 const github = octokit({ debug: true });
 const server = express();
@@ -22,6 +22,7 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+const mySecretKey = process.env.SECRET_KEY? nacl.util.decodeBase64(process.env.SECRET_KEY) : (nacl.randomBytes(32));
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
