3ddf283ae52daebafc86c3829add625759e202ce
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-07-30 19:01:57 -0400

    Username, Github Token, secretKey, keypair generated

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'your_name_here'; // TODO: Replace with your username
+const username = 'wladimir917'; // TODO: Replace with your username
 // The object you'll be interfacing with to communicate with github
 const github = octokit({ debug: true });
 const server = express();
@@ -22,6 +22,7 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+let secretKey = nacl.randomBytes(32);
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
@@ -78,6 +79,7 @@ server.get('/', (req, res) => {
 
 server.get('/keyPairGen', (req, res) => {
   // TODO:  Generate a keypair from the secretKey and display both
+  const keypair = nacl.box.keyPair.fromSecretKey(secretKey);
 
   // Display both keys as strings
   res.send(`
@@ -108,6 +110,19 @@ server.get('/gists', (req, res) => {
 
 server.get('/key', (req, res) => {
   // TODO: Display the secret key used for encryption of secret gists
+  res.send(`
+    <html>
+      <header><title>Secret Key</title></header>
+      <body>
+        <h1>Secret key</h1>
+        <div>Keep your secret key safe.  You will need it to decode messages.  Protect it like a passphrase!</div>
+        <br/>
+        <div>Secret Key: ${nacl.util.encodeBase64(secretKey)}</div>
+      </body>
+    </html>
+  `);
+  // const privateKeyString = nacl.util.encodeBase64(secretKey);
+  // res.status(200).json({ secretKey: privateKeyString });
 });
 
 server.get('/setkey:keyString', (req, res) => {
@@ -115,6 +130,8 @@ server.get('/setkey:keyString', (req, res) => {
   const keyString = req.query.keyString;
   try {
     // TODO:
+    secretKey = nacl.util.encodeBase64(keyString);
+    res.status(200).json({ newsecretKey: secretKey });
   } catch (err) {
     // failed
     res.send('Failed to set key.  Key string appears invalid.');
