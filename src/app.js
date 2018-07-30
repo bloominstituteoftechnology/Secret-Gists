e205891c97d6a1860b013f61bff89fb859a508a5
Danny Vail <danny_vail@outlook.com>
2018-07-30 17:00:29 -0400

    Project setup - Added GitHub token to environment variables - Added GitHub username as app constant - Added an empty keyPair object to allow server to start up - Added nodemon to handle server restart

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -6,7 +7,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'your_name_here'; // TODO: Replace with your username
+const username = 'd-vail';
 const github = octokit({ debug: true });
 const server = express();
 
@@ -21,6 +22,10 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+const keypair = {
+  publicKey: '',
+  secretKey: ''
+};
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
