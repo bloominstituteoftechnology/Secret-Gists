cd2a7e25e1e65b831a429b3c014b9aa7e137ef8b
Lavell Burton <taylormade800@gmail.com>
2018-07-30 15:52:41 -0700

    pushing changes

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+const keypair = {};
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
@@ -176,3 +177,4 @@ server.listen(3000);
 
 
 // initial commit going over docs and implementation
+
