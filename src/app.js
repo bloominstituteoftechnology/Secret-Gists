82c975c43fd265ed9c071cf1f427bb1418a62616
Sean Chen <skypemaster007@gmail.com>
2018-07-30 10:16:25 -0500

    Add a comment to skeleton code

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
 const username = 'your_name_here'; // TODO: Replace with your username
+// The object you'll be interfacing with to communicate with github
 const github = octokit({ debug: true });
 const server = express();
 
