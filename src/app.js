cbafdfd270e3bbf977e9a9f101230f6c370e0f01
kelly <kelfro@fastmail.com>
2018-07-30 13:23:25 -0700

    trying to load key, correcting errors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'your_name_here'; // TODO: Replace with your username
+const username = 'kelfro'; // DONE: Replace with your username
 // The object you'll be interfacing with to communicate with github
 const github = octokit({ debug: true });
 const server = express();
@@ -22,6 +22,16 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+let secretKey; // the linter made me do this
+try {
+  const data = fs.readFileSync('./config.json'); // get the info
+  const temp = JSON.parse(data); // jsonify it
+  secretKey = nacl.util.decodeBase64(temp.SECRET_KEY);
+} catch (err) {
+  secretKey = nacl.randomBytes(32);
+  const temp = { SECRET_KEY: nacl.util.encodeBase64(secretKey) };
+  fs.writeFileSync('./config.json', JSON.stringify(temp));
+}
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
@@ -78,7 +88,7 @@ server.get('/', (req, res) => {
 
 server.get('/keyPairGen', (req, res) => {
   // TODO:  Generate a keypair from the secretKey and display both
-
+  const keypair = nacl.box.keyPair(); // I kept getting error messages saying there was no such thing as keypair so I made one up
   // Display both keys as strings
   res.send(`
     <html>
@@ -141,6 +151,10 @@ server.post('/create', urlencodedParser, (req, res) => {
 server.post('/createsecret', urlencodedParser, (req, res) => {
   // TODO:  Create a private and encrypted gist with given name/content
   // NOTE - we're only encrypting the content, not the filename
+  const { name, content } = req.body
+
+  let nonce = nacl.randomBytes(nacl.secretbox.nonceLength);
+
 });
 
 server.post('/postmessageforfriend', urlencodedParser, (req, res) => {
