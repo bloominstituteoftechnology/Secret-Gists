31255aac40a8b15ea34f3b6688f8d93a413e3de9
Xang Thao <xangthao24@yahoo.com>
2018-07-30 14:04:08 -0700

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'your_name_here'; // TODO: Replace with your username
+const username = 'xangthao24'; // TODO: Replace with your username
 // The object you'll be interfacing with to communicate with github
 const github = octokit({ debug: true });
 const server = express();
