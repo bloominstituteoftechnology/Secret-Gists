fc32a44c1a0c391126c331b33d77915b5d4cca51
Zach Campbell <beazlebubbles@gmail.com>
2018-07-30 20:45:34 -0600

    Attempted to load config.json

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-console */
 
-require('dotenv').config();
-const fs = require('fs');
+// require('dotenv').config(); This didn't work for me, so I just used bash
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'your_name_here'; // TODO: Replace with your username
+const username = 'wajnurfes'; // TODO: Replace with your username
 // The object you'll be interfacing with to communicate with github
 const github = octokit({ debug: true });
 const server = express();
@@ -18,13 +18,23 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Generate an access token: https://github.com/settings/tokens
 // Set it to be able to create gists
-console.log(process.env.GITHUB_TOKEN)
+
 github.authenticate({
   type: 'oauth',
   token: process.env.GITHUB_TOKEN
 });
 
+let config
+
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+try {
+  config = require('./config.json');
+}
+catch (err) {
+  config = { "My spoon is too big": "I am a banana" }
+}
+console.log(config)
+
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
