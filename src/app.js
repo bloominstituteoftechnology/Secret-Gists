5a42f1a0a7e8c2ee60f96ed0bcb9e47d2561378c
honda0306 <christopher.honda92@gmail.com>
2018-07-31 13:09:05 -0700

    Updated code based on lecture notes; moved username to .env file for security

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'honda0306'; // TODO: Replace with your username
+const username = process.env.GITHUB_USERNAME; // TODO: Replace with your username
 // The object you'll be interfacing with to communicate with github
 const github = octokit({ debug: true });
 const server = express();
@@ -23,7 +23,31 @@ github.authenticate({
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
 // const keypair = {};
-const secretKey = nacl.randomBytes(32);
+// const secretKey = nacl.randomBytes(32);
+let secretKey; // Our secret key as a Uint8Array
+
+try {
+  // try to read the config file
+  const data = fs.readFileSync('./config.json');
+  // parse the data that we read from the json file
+  const keyObject = JSON.parse(data);
+  // saves key
+  secretKey = nacl.util.decodeBase64(keyObject.secretKey);
+} catch (error) {
+  const keyObject = nacl.box.keyPair();
+  // secretKey = nacl.randomBytes(32);
+  // console.log('secretKey base 64: ', nacl.util.encodeBase64(secretKey));
+  // console.log('secretKey UTF8: ', nacl.util.encodeUTF8(secretKey));
+  const keyObject = { secretKey: nacl.util.encodeBase64(secretKey) };
+
+  // write this keyObject to config.json
+  fs.writeFile('./config.json', JSON.stringify(keyObject), (ferr) => {
+    if (ferr) {
+      // console.log('Error writing secretKey to config file: ', ferr.message);
+      return;
+    }
+  });
+}
 
 
 server.get('/', (req, res) => {
@@ -111,7 +135,7 @@ server.get('/gists', (req, res) => {
 
 server.get('/key', (req, res) => {
   // TODO: Display the secret key used for encryption of secret gists
-  res.json({ newSecretKey: nacl.util.encodeBase64(secretKey) });
+  res.send(nacl.util.encodeBase64(secretKey));
 });
 
 server.get('/setkey:keyString', (req, res) => {
@@ -119,6 +143,15 @@ server.get('/setkey:keyString', (req, res) => {
   const keyString = req.query.keyString;
   try {
     // TODO:
+    // Set secretKey to be whatever the user passes through input
+    secretKey = nacl.util.decodeBase64(keyString);
+    const keyObject = { secretKey: keyString };
+    fs.writeFile('./config.json', JSON.stringify(keyObject), (ferr) => {
+      if (ferr) {
+        return;
+      }
+    });
+    res.send(`<div>Key set to new value: ${secretKey}</div>`);
   } catch (err) {
     // failed
     res.send('Failed to set key.  Key string appears invalid.');
@@ -145,10 +178,18 @@ server.post('/create', urlencodedParser, (req, res) => {
 server.post('/createsecret', urlencodedParser, (req, res) => {
   // TODO:  Create a private and encrypted gist with given name/content
   // NOTE - we're only encrypting the content, not the filename
-
+  // Read the name and content off the url params
   const { name, content } = req.body;
-  const files = { [name]: { content } };
-  github.gists.create({ files, public: false })
+  // initialize nonce
+  const nonce = nacl.randomBytes(24);
+  // decode UTF8 content, then encrypt contents of gist
+  const ciphertext = nacl.secretbox(nacl.util.decodeUTF8(content), nonce, secretKey);
+  // Make nonce persistent until decryption at a later time
+  const blob = nacl.util.encodeBase64(nonce) + nacl.util.encodeBase64(ciphertext);
+  // format blob and name in format for Github API
+  const file = { [name]: { content: blob } };
+  // send POST to Github
+  github.gists.create({ files: file, public: false })
     .then((response) => {
       res.json(response.data);
     })
