4b936fb0fca088fdda44351aafd08a49b9a8a0a0
udykas <udykas@gmail.com>
2018-07-31 17:09:30 -0400

    github token is in env file and app is loading in browser now

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+const secretKey = nacl.randomBytes(32);
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
@@ -80,7 +81,7 @@ server.get('/', (req, res) => {
 
 server.get('/keyPairGen', (req, res) => {
   // TODO:  Generate a keypair from the secretKey and display both
-
+  const keypair = nacl.box.keyPair.fromSecretKey(secretKey);
   // Display both keys as strings
   res.send(`
     <html>
