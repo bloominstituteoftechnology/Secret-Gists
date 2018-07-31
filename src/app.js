a9c8c4aaa8cfe8451ea25d35567f0631fb16a14f
FireInjun <mk@FireInjun.com>
2018-07-31 02:28:14 -0400

    7/31/2018 2:28 AM

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'FireInjun'; // TODO: Replace with your username
+const username = process.env.GITHUB_USERNAME; // TODO: Replace with your username
 // The object you'll be interfacing with to communicate with github
 const github = octokit({ debug: true });
 const server = express();
