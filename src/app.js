1ff9cbaaf9f60dbe0147998e9a5ea612121d8ec8
Anne Courtney <annecourtney_1@yahoo.com>
2018-07-31 17:23:49 -0500

    can retrieve encrypted gist with ids

--- src/app.js
+++ src/app.js
@@ -191,17 +191,17 @@ server.get('/setkey:keyString', (req, res) => {
 server.get('/fetchmessagefromself:id', (req, res) => {
   // TODO:  Retrieve and decrypt the secret gist corresponding to the given ID
   const id = req.query.id;
-
   github.gists.get({ id })
     .then((response) => {
       const gist = response.data;
-      const filename = Object.keys(gist.giles)[0];
+      const filename = Object.keys(gist.files)[0];
       const blob = gist.files[filename].content;
       let nonce = blob.slice(0, 32);
       let ciphertext = blob.slice(32, blob.length);
       nonce = nacl.util.decodeBase64(nonce);
       ciphertext = nacl.util.decodeBase64(ciphertext);
-      const plaintext = nacl.secretbox.open(ciphertext, nonce, secretKey);
+      const key = nacl.util.decodeBase64(process.env.SECRET_KEY);
+      const plaintext = nacl.secretbox.open(ciphertext, nonce, key);
       res.send(nacl.util.encodeUTF8(plaintext));
     })
     .catch((err) => {
