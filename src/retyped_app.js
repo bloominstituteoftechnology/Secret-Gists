78ecd4ecc1051ad044eab4059744f6f15056a390
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-08-06 14:17:06 -0700

    feat: add get /, get /keyPairGen,

--- src/retyped_app.js
+++ src/retyped_app.js
@@ -27,4 +27,98 @@ github.authenticate({
 // TODO: Attempt to load the key from config.json. If it is not found, create a new 32 byte key.
 let secretKey; // Our secret key as a Unit8Array
 
+// 1. Try to read the config.json file
+try {
+  // try to read the config file
+  const data = fs.readFileSync('./config.json');
+  // parse the data that we read from the json file
+  const keyObject = JSON.parse(data);
+  // 2. If the config.json exists and the key is in there, initialize `secretKey` variable to be the value in the file
+  secretKey = nacl.util.decodeBase64(keyObject.secretKey);
+} catch (err) {
+  // 3. If we fail to read the config.json, generate a new random secretKey
+  secretKey = nacl.randomBytes(32);
+  // create the keyObject, encoding the secretKey as a string
+  const keyObject = { secretKey: nacl.util.encodeBase64(secretKey)};
+  // write this keyObject to config.json
+  fs.writeFile('./config.json', JSON.stringify(keyObject), (ferr) => {
+    if (ferr) {
+      console.log('Error writing secret key to config file: ', ferr.message);
+      return;
+    }
+  });
+}
+
+server.get('/', (req, res) => {
+  // Return a response that documents the other routes/operations available
+  res.send(`
+    <html>
+      <header><title>Secret Gists!</title></header>
+      <body>
+        <h1>Secret Gists!</h1>
+        <div>This is an educational implementation.  Do not use for truly valuable information</div>
+        <h2>Supported operations:</h2>
+        <ul>
+          <li><i><a href="/keyPairGen">Show Keypair</a></i>: generate a keypair from your secret key.  Share your public key for other users of this app to leave encrypted gists that only you can decode with your secret key.</li>
+          <li><i><a href="/gists">GET /gists</a></i>: retrieve a list of gists for the authorized user (including private gists)</li>
+          <li><i><a href="/key">GET /key</a></i>: return the secret key used for encryption of secret gists</li>
+        </ul>
+        <h3>Set your secret key to a specific key</h3>
+        <form action="/setkey:keyString" method="get">
+          Key String: <input type="text" name="keyString"><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h3>Create an *unencrypted* gist</h3>
+        <form action="/create" method="post">
+          Name: <input type="text" name="name"><br>
+          Content:<br><textarea name="content" cols="80" rows="10"></textarea><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h3>Create an *encrypted* gist for yourself</h3>
+        <form action="/createsecret" method="post">
+          Name: <input type="text" name="name"><br>
+          Content:<br><textarea name="content" cols="80" rows="10"></textarea><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h3>Retrieve an *encrypted* gist you posted for yourself</h3>
+        <form action="/fetchmessagefromself:id" method="get">
+          Gist ID: <input type="text" name="id"><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h3>Create an *encrypted* gist for a friend to decode</h3>
+        <form action="/postmessageforfriend" method="post">
+          Name: <input type="text" name="name"><br>
+          Friend's Public Key String: <input type="text" name="publicKeyString"><br>
+          Content:<br><textarea name="content" cols="80" rows="10"></textarea><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h3>Retrieve an *encrypted* gist a friend has posted</h3>
+        <form action="/fetchmessagefromfriend:messageString" method="get">
+          String From Friend: <input type="text" name="messageString"><br>
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+server.get('/keyPairGen', (req, res) => {
+  // TODO: Generate a keypair from the secretKey and display both
+  const keypair = nacl.box.keyPair.fromSecretKey(secretKey);
+
+  // Display both keys as strings
+  res.send(`
+    <html>
+      <header><title>Keypair</title></header>
+      <body>
+        <h1>Keypair</h1>
+        <div>Share your public key with anyone you want to be able to leave you secret messages.</div>
+        <div>Keep your secret key safe.  You will need it to decode messages.  Protect it like a passphrase!</div>
+        <br/>
+        <div>Public Key: ${nacl.util.encodeBase64(keypair.publicKey)}</div>
+        <div>Secret Key: ${nacl.util.encodeBase64(keypair.secretKey)}</div>
+      </body>
+    </html>
+  `);
+})
 
