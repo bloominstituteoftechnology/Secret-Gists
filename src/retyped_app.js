2eb6083b52063b10adfa90fff2a14553cc728b05
Jacob Henry <jacobiswinning@gmail.com>
2018-08-06 14:34:30 -0600

    for reals this time

--- src/retyped_app.js
+++ src/retyped_app.js
@@ -96,3 +96,22 @@ server.get('/', (req, res) => {
     </html>
   `);
 });
+
+server.get('/keyPairGen', (req, res) => {
+  // TODO:  Generate a keypair from the secretKey and display both
+  const keypair = nacl.box.keyPair();
+  // Display both keys as strings
+  res.send(`
+    <html>
+      <header><title>Keypair</title></header>
+      <body>
+        <h1>Keypair</h1>
+        <div>Share your public key with anyone you want to be able to leave you secret messages.</div>
+        <div>Keep your secret key safe.  You will need it to decode messages.  Protect it like a passphrase!</div>
+        <br/>
+        <div>Public Key: ${nacl.util.encodeBase64(keypair.publicKey)}</div>
+        <div>Secret Key: ${nacl.util.encodeBase64(keypair.secretKey)}</div>
+      </body>
+    </html>
+  `);
+});
