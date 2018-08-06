a23cb0fcefa6cd243d7451d9d352137d2d4e8417
april <martinezapril56@yahoo.com>
2018-08-06 16:06:32 -0400

    rewrite

--- src/retyped_app.js
+++ src/retyped_app.js
@@ -1,7 +1,12 @@
-require('dotenv').config();// pulling in the info from the env file 
+require('dotenv').config(); // pulling in the info from the env file
 const fs = require('fs');
-const bodyParser = require( 'body-parser' );
-const express = require( 'express' );
-const octokit = require( '@octokit/rest' );//family of client libraries for the GitHub API
-const nacl = require( 'tweetnacl' );//pulls in high-security cryptographic library.
-nacl.util = require( 'tweetnacl-util' );//pulls in high-security cryptographic library.
+const bodyParser = require('body-parser');
+const express = require('express');
+const octokit = require('@octokit/rest'); //family of client libraries for the GitHub API
+const nacl = require('tweetnacl'); //pulls in high-security cryptographic library.
+nacl.util = require('tweetnacl-util'); //pulls in high-security cryptographic library.
+
+const username = 'April7229';  //putting in my username. 
+const github = octokit({
+  debug: true
+});      //oks like it is pulling the octokit library and setting it debug to true
