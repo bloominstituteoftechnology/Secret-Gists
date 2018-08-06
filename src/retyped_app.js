155f7a186e6e92351b8c7c36a9b2f1d111b8c772
Jacob Henry <jacobiswinning@gmail.com>
2018-08-06 14:32:42 -0600

    added several methods to handle auth and a try catch to handle encryption errors

--- src/retyped_app.js
+++ src/retyped_app.js
@@ -9,3 +9,90 @@ nacl.util = require('tweetnacl-util');
 
 // code
 const username = 'Blast3d'; // Added my username so the program can access my gists account.
+
+// The object you'll be interfacing with to communicate with github
+const github = octokit({ // sets octokit to a var, sets it to debug mode
+  debug: true
+});
+const server = express(); // creates var that sets up express
+
+// Create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
+
+// Generate an access token: https://github.com/settings/tokens
+// Set it to be able to create gists
+github.authenticate({
+  type: 'oauth',
+  token: process.env.GITHUB_TOKEN
+});
+
+// TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+let secretKey;
+try {
+  const data = fs.readFileSync('./config.json'); // uses file system to read the config file
+  const keyObject = JSON.parse(data); // parses json String value or object that is in string form so it can be returned
+  secretKey = nacl.util.decodeBase64(keyObject.secretKey); // fixed this line to  use nacl to decode base 64 to utf8 before it was just using the secre
+} catch (err) { // catches possible error then 
+  //deleted redundant code
+  fs.writeFile('./config.json', JSON.stringify(keyObject), (error) => {
+    if (error) {
+      console.log('error:', error.message);
+      return error;
+    }
+  });
+}
+
+server.get('/', (req, res) => {
+  // Return a response that documents the other routes/operations available
+  res.send(`
+    <html>
+      <header><title>Secret Gists!</title></header>
+      <body>
+        <h1>Secret Gists!</h1>
+        <div>This is an educational implementation.  Do not use for truly valuable information</div>
+        <h2>Supported operations:</h2>
+        <ul>
+          <li><i><a href="/keyPairGen">Show Keypair</a></i>: generate a keypair from your secret key.  Share your public key for other users of this app to leave encrypted gists that only you can decode with your secret key.</li>
+          <li><i><a href="/gists">GET /gists</a></i>: retrieve a list of gists for the authorized user (including private gists)</li>
+          <li><i><a href="/key">GET /key</a></i>: return the secret key used for encryption of secret gists</li>
+        </ul>
+        <h3>Set your secret key to a specific key</h3>
+        <form action="/setkey:keyString" method="get">
+          Key String: <input type="text" name="keyString"><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h3>Create an *unencrypted* gist</h3>
+        <form action="/create" method="post">
+          Name: <input type="text" name="name"><br>
+          Content:<br><textarea name="content" cols="80" rows="10"></textarea><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h3>Create an *encrypted* gist for yourself</h3>
+        <form action="/createsecret" method="post">
+          Name: <input type="text" name="name"><br>
+          Content:<br><textarea name="content" cols="80" rows="10"></textarea><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h3>Retrieve an *encrypted* gist you posted for yourself</h3>
+        <form action="/fetchmessagefromself:id" method="get">
+          Gist ID: <input type="text" name="id"><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h3>Create an *encrypted* gist for a friend to decode</h3>
+        <form action="/postmessageforfriend" method="post">
+          Name: <input type="text" name="name"><br>
+          Friend's Public Key String: <input type="text" name="publicKeyString"><br>
+          Content:<br><textarea name="content" cols="80" rows="10"></textarea><br>
+          <input type="submit" value="Submit">
+        </form>
+        <h3>Retrieve an *encrypted* gist a friend has posted</h3>
+        <form action="/fetchmessagefromfriend:messageString" method="get">
+          String From Friend: <input type="text" name="messageString"><br>
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+  `);
+});
