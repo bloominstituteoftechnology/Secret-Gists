09e7feecaf881d974d20530e835b61150adcfd93
Thuy Pham <tpham045@hotmail.com>
2018-09-12 20:19:27 -0700

    Get gist
    - Add Github Token and username

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'your_name_here'; // TODO: Replace with your username
+const username = 'iamthuypham';
 // The object you'll be interfacing with to communicate with github
 const github = octokit({ debug: true });
 const server = express();
@@ -24,6 +24,7 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+const keypair = {};
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
